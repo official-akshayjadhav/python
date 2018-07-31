3a4afcc2aeb207f6c1c9e59f7899b8ab30528e7e
official-2 <official.akshaymjadhav@gmail.com>
2018-07-31 16:49:17 +0530

    updated 31-07-2018

---
+++ Apps/Speech_Recognition/commandFunctions.py
@@ -0,0 +1,7 @@
+import webbrowser
+
+
+#opens specified link in the browser to which we set as default
+def open_browser(str):
+    webbrowser.open(str)
+#end open_browser
---
+++ Practice/loops/forloop.py
@@ -0,0 +1,7 @@
+# Paste your code into this box
+count = 0 ;
+for char in s:
+  if (char == 'a' or char == 'e' or char == 'i' or char == 'o' or char == 'u') :
+    count += 1;
+
+print("Numner of vowels: "+str(count))
