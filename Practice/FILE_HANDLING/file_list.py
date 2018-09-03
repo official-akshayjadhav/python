7e27ef7d831d8249b13bdfe2f44b9aa411717a05
akshay <official.akshayjadhav@gmail.com>
2018-09-04 04:41:37 +0530

    upated 04-09-2018

---
+++ Practice/FILE_HANDLING/file_list.py
@@ -0,0 +1,6 @@
+import os
+
+file_list = os.listdir("/home/akshay");
+
+for file in file_list:
+	print(file) 
---
+++ Practice/SHAPES/square.py
@@ -0,0 +1,11 @@
+import turtle
+
+def draw_square():
+	window = turtle.Screen()
+	pen = turtle.Turtle()
+	pen.forward(200)
+	pen.downward(200)
+	window.exitonclick()
+
+
+draw_square()
